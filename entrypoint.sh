17b9ca2292ff62c5dbe520d22f1c0a291842ed8c
MaxDrg <maximadda2001@gmail.com>
2022-04-15 19:08:34 +0200

    mi_forum 0.0.3

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,5 @@
 #!/bin/sh
 
-if [ "$DATABASE" = "postgres" ]
-then
-    echo "Waiting for postgres..."
-
-    while ! nc -z $HOST $PORT; do
-      sleep 0.1
-    done
-
-    echo "PostgreSQL started"
-fi
-
 python manage.py flush --no-input
 python manage.py makemigrations
 python manage.py migrate
