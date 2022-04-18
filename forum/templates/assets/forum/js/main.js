8d886ba5127a6896e27c330e88d9eff9dc204cd6
MaxDrg <maximadda2001@gmail.com>
2022-04-18 11:09:16 +0200

    mi_forum 0.0.5

--- forum/templates/assets/forum/js/main.js
+++ forum/templates/assets/forum/js/main.js
@@ -21,3 +21,13 @@ exampleModal.addEventListener('show.bs.modal', function (event) {
   var modalTitle = exampleModal.querySelector('.modal-title')
   modalTitle.textContent = 'Оформить ' + '"' + recipient + '"'
 })
+
+function showArea(block_id) {
+  let textareas = document.getElementsByClassName('comments_textareas');
+  for (var i = 0; i < textareas.length; i++) {
+      var current_text = textareas[i];
+      current_text.style.display = 'none';
+  }
+  let text = document.getElementById('block_' + block_id);
+  text.style.display = 'block';
+};
