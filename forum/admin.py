a417e16467dc39224f94bd30e722caff401c172c
MaxDrg <maximadda2001@gmail.com>
2022-04-21 16:11:08 +0200

    mi_forum 0.0.7

--- forum/admin.py
+++ forum/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import User, New, Category, Forum
+from .models import New, Category, Forum
 
-admin.site.register(User)
 admin.site.register(New)
 admin.site.register(Category)
 admin.site.register(Forum)
