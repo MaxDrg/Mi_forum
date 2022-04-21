fce9b0c70e77b5031bb98e5a0be4c37ccc046815
MaxDrg <maximadda2001@gmail.com>
2022-04-21 18:23:46 +0200

    mi_forum 0.0.7

--- forum/templates/assets/forum/js/main.js
+++ forum/templates/assets/forum/js/main.js
@@ -17,7 +17,7 @@ function search_info() {
   let input = document.getElementById('searchbar').value
   input=input.toLowerCase(); 
 
-  let x = document.getElementsByClassName('newsItem');
+  let x = document.getElementsByClassName('newsItem__info');
   for (i = 0; i < x.length; i++) {
       if (!x[i].innerHTML.toLowerCase().includes(input)) { 
           x[i].style.display="none";
