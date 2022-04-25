71bccf3e83fa364e774b74d26d85116b0040af47
MaxDrg <maximadda2001@gmail.com>
2022-04-25 21:31:04 +0200

    mi_forum 0.0.8

--- mi_forum/settings.py
+++ mi_forum/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['m3.fan']
+ALLOWED_HOSTS = ['localhost', 'm3.fan']
 
 CSRF_TRUSTED_ORIGINS = [str(os.environ.get('CSRF_TRUSTED_ORIGINS'))]
 
