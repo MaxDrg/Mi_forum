973aa4d580c1a34647e3470f699c22c3c23c9fe7
MaxDrg <maximadda2001@gmail.com>
2022-05-01 10:49:06 +0200

    mi_forum 1.0.0

--- forum/templates/assets/forum/js/main.js
+++ forum/templates/assets/forum/js/main.js
@@ -50,7 +50,7 @@ function change_price() {
 $(function(){
   $('.minimized').click(function(event) {
     var i_path = $(this).attr('src');
-    $('body').append('<div id="overlay"></div><div id="magnify"><img src="'+i_path+'"><div id="close-popup"><i></i></div></div>');
+    $('body').append('<div id="overlay"><div id="magnify"><img src="'+i_path+'"><div id="close-popup"><i></i></div></div></div>');
     $('#magnify').css({
      left: ($(document).width() - $('#magnify').outerWidth())/2,
      // top: ($(document).height() - $('#magnify').outerHeight())/2 upd: 24.10.2016
