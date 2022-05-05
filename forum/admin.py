4679e4916e6c9d5dabb9c8f9b61ae051ee172898
MaxDrg <maximadda2001@gmail.com>
2022-05-05 21:01:16 +0200

    mi_forum 1.2.1

--- forum/admin.py
+++ forum/admin.py
@@ -5,4 +5,4 @@ from .models import New, Category, Forum, Vocabulary_word
 admin.site.register(New)
 admin.site.register(Category)
 admin.site.register(Forum)
-admin.site,register(Vocabulary_word)
+admin.site.register(Vocabulary_word)
